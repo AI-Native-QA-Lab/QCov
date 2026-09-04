84c01daefddb317f5d3ec9af975a41c4d7d4afc9
Nao <dengnao@gmail.com>
2026-09-04 12:19:08 +0800

    feat: add project configuration loading

--- qcov/models/__init__.py
+++ qcov/models/__init__.py
@@ -1,5 +1,13 @@
 """Versioned QCov protocol models and file loaders."""
 
+from .config import ProjectConfig, ResolvedPaths, resolve_paths
 from .protocol import QualityEvidence, QualityPolicy, TestingObligation
 
-__all__ = ["QualityEvidence", "QualityPolicy", "TestingObligation"]
+__all__ = [
+    "ProjectConfig",
+    "QualityEvidence",
+    "QualityPolicy",
+    "ResolvedPaths",
+    "TestingObligation",
+    "resolve_paths",
+]
